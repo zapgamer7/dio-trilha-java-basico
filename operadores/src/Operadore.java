b17bd96884520529e2ae4105cd3b83830a52c431
cauan <cauan358@gmail.com>
2023-10-03 23:16:20 -0300

    trabalhando com operadores logicos

--- operadores/src/Operadore.java
+++ operadores/src/Operadore.java
@@ -1,28 +1,14 @@
 public class Operadore {
     public static void main(String[] args) throws Exception {
+        Boolean condicao1 = true;
+        Boolean condicao2 = false;
 
-        String concatenação = "?";
-
-        concatenação = 1+1+1+"1";
-        
-        System.out.println(concatenação);
-
-        concatenação = 1+"1"+1+1;
-
-        System.out.println(concatenação);
-
-        concatenação = 1+"1"+1+"1";
-
-        System.out.println(concatenação);
-
-        concatenação = "1"+1+1+1;
-
-        System.out.println(concatenação);
-
-        concatenação = "1"+(1+1+1);
-
-        System.out.println(concatenação);
-
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições são verdadeiras");
+        }
 
 
     }    
