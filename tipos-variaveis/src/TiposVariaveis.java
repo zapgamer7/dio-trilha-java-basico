b050aadf22a9a7d2871b1c2457289326939d0ffb
cauan <cauan358@gmail.com>
2023-10-03 22:39:13 -0300

    criação do projeto

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        Short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        final Double PI = 3.14;
+
+        System.out.println(PI);
     }
 }
