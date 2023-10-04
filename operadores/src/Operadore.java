677bf5f0a6e52191a2401c8696168b38c6b1fc91
cauan <cauan358@gmail.com>
2023-10-03 22:50:12 -0300

    concatenação entre numeros e strigns

---
+++ operadores/src/Operadore.java
@@ -0,0 +1,29 @@
+public class Operadore {
+    public static void main(String[] args) throws Exception {
+
+        String concatenação = "?";
+
+        concatenação = 1+1+1+"1";
+        
+        System.out.println(concatenação);
+
+        concatenação = 1+"1"+1+1;
+
+        System.out.println(concatenação);
+
+        concatenação = 1+"1"+1+"1";
+
+        System.out.println(concatenação);
+
+        concatenação = "1"+1+1+1;
+
+        System.out.println(concatenação);
+
+        concatenação = "1"+(1+1+1);
+
+        System.out.println(concatenação);
+
+
+
+    }    
+}
